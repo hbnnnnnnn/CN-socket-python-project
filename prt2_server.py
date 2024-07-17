7ca4004fd9e5dcf042869b37e67242d2e945ea23
Ha Bao Ngoc <hiembaonon@gmail.com>
2024-07-17 14:24:49 +0700

    Update prt2_server.py

--- prt2_server.py
+++ prt2_server.py
@@ -1,8 +1,6 @@
-import queue
 import socket
 import threading
 import os
-import sys
 
 HEADER = 64
 FORMAT = 'utf-8'
@@ -11,24 +9,23 @@ HOST = socket.gethostbyname(socket.gethostname())
 FILE_LIST_PATH = 'file_list.txt'
 DELIMITER = ' '
 
-CHUNKS_SIZE = 1024
-NORMAL = 1
-HIGH = 4 * NORMAL
-CRITICAL = 10 * NORMAL
-
-stop_event = threading.Event()
-download_queue = queue.Queue()
+CHUNK_SIZE = 1024
+PRIORITY = {
+    "NORMAL": 1,
+    "HIGH": 4,
+    "CRITICAL": 10
+}
 
 def load_file_list():
     with open(FILE_LIST_PATH, 'r') as file:
         return file.read()
 
-def is_exist(filename):
+def file_exists(filename):
     with open(FILE_LIST_PATH, 'r') as file:
         file_list = [line.split()[0] for line in file.read().splitlines()]
     return filename in file_list and os.path.exists(FILE_LIST_PATH)
 
-def create_protocol(method, data):
+def apply_protocol(method, data):
     message = f"{method}{DELIMITER}{data}"
     message_encoded = message.encode(FORMAT)
     msg_length = len(message_encoded)
@@ -37,36 +34,8 @@ def create_protocol(method, data):
     protocol_message = header + message_encoded
     return protocol_message
 
-def update_queue(client, file_list):
-    for file in file_list.splitlines():
-        filename, priority = file.split()
-        download_queue.put((client, filename, priority))
 
-def handle_queue():
-    while not stop_event.is_set():
-        try:
-            client, filename, priority = download_queue.get(timeout=2)
-        except queue.Empty:
-            continue
-        if is_exist(filename):
-            client.sendall(create_protocol("SEN", "OK"))
-            with open(filename, 'rb') as output:
-                chunks = output.read(CHUNKS_SIZE)
-                while chunks:
-                    for _ in range(globals()[priority]):
-                        if not chunks:
-                            break
-                        client.sendall(create_protocol("PROC",chunks))
-                        chunks = output.read(CHUNKS_SIZE)
-                client.sendall(create_protocol("END",""))
-        else:
-            client.sendall(create_protocol("ERR", filename))
-
-
-def handle_client(client, addr):
-    print(f"[NEW CONNECTION] A new connection is accepted from {addr}")
-    file_list = load_file_list()
-    client.sendall(create_protocol("SEN", file_list))
+def update_list(client, addr, download_list):
     while True:
         try:
             str_header = client.recv(HEADER).decode(FORMAT)
@@ -75,16 +44,57 @@ def handle_client(client, addr):
             msg_length = int(str_header.split(DELIMITER)[1])
             message = client.recv(msg_length).decode(FORMAT)
             method, data = message.split(DELIMITER, 1)
-            if method == "DIS":
-                stop_event.set()
-                break
             if method == "GET":
-                data = data.strip(DELIMITER)
-                update_queue(client, data)
+                filename, priority = data.split(DELIMITER)
+                sent = 0
+                if file_exists(filename):
+                    client.sendall(apply_protocol("SEN", "OK" + DELIMITER + str(os.path.getsize(filename))))
+                    download_list.append((filename, priority, sent))
+                else:
+                    print(f"[ERROR] {filename} requested from {addr} does not exist!")
+                    client.send(apply_protocol("ERR", filename))
+        except Exception as e: 
+            print(f"Error {e}")
+            continue
 
+def process_list(client, addr, download_list):
+    while True:
+        try:
+            for i in range(len(download_list)):
+                filename, priority, sent = download_list[i]
+                with open(filename, 'rb') as output:
+                    output.seek(sent * CHUNK_SIZE)
+                    priority = PRIORITY.get(priority, 0)
+                    for _ in range(priority):
+                        chunk = output.read(CHUNK_SIZE)
+                        if not chunk:
+                            client.sendall(apply_protocol("SEN","END"))
+                            print(f"[SEND] Sent {filename} to {addr} successfully!")
+                            download_list.pop(i)
+                            i -= 1
+                            break
+                        client.sendall(apply_protocol("SEF", filename + DELIMITER + chunk))
+                    
+        except Exception as e: 
+            print(f"Error {e}")
+            continue
+
+
+def handle_client(client, addr):
+    print(f"[NEW CONNECTION] A new connection is accepted from {addr}")
+    file_list = load_file_list()
+    client.sendall(apply_protocol("SEN", file_list))
+    download_list = []
+    while True:
+        try:
+            list_process = threading.Thread(target=process_list,args=(client, addr, download_list))
+            list_update = threading.Thread(target=update_list,args=(client, addr, download_list))
+            list_process.start()
+            list_update.start()
         except Exception as e:
             print(f"Error: {e}")
             break
+    print(f"[DISCONNECTED] {addr} has disconnected!")
     client.close()
 
 def start_server():
@@ -96,8 +106,6 @@ def start_server():
             while True:
                 client, addr = server.accept()
                 client_handler = threading.Thread(target=handle_client, args=(client, addr))
-                queue_handler = threading.Thread(target=handle_queue)
-                queue_handler.start()
                 client_handler.start()
         except Exception as e:
             print(f"ERROR: {e}")
