515883086b4beb830ff9c7d3c94a7ceda493a790
Lam Vi Khang <lvkhang23@clc.fitus.edu.vn>
2024-07-26 19:59:25 +0700

    Update prt2_client.py

--- prt2_client.py
+++ prt2_client.py
@@ -5,18 +5,21 @@ import signal
 import threading
 import time
 from time import sleep
+import os
+
+PORT = 1603
+HOST = socket.gethostbyname(socket.gethostname())
 
 HEADER = 64
 FORMAT = "utf-8"
 CHUNK_SIZE = 1024
-PORT = 1603
-HOST = socket.gethostbyname(socket.gethostname())
 INPUT_FILE = "input.txt"
 DELIMITER = ' '
-PROCESSED_TRACKER = 0
-FILE_LIST = ""
 
 DOWNLOADS = []
+PROCESSED_TRACKER = 0
+FILE_LIST = []
+
 shutdown_event = threading.Event()
 
 def apply_protocol(method, message):
@@ -29,28 +32,19 @@ def apply_protocol(method, message):
 
     return header + message_encoded
 
-# def disconnect(sig, frame, conn):
-#     print("SIGINT received, shutting down gracefully...")
-#     shutdown_event.set()
-#     try:
-#         conn.close()
-#     except Exception as e:
-#         print(f"Error closing connection: {e}")
-#     print("Disconnect successfully!")
-#     sys.exit(0)
-
-# def setup_signal_handler(conn):
-#     signal.signal(signal.SIGINT, lambda sig, frame: disconnect(sig, frame, conn))
-
-def disconnect(sig, frame):
-    # print("\nSIGINT received, shutting down gracefully...")
-    
+def disconnect(sig, frame):    
     shutdown_event.set()
-
     for file in DOWNLOADS:
-        file[1].clear()
-
-    sys.exit()
+        file[1].close()
+    
+    # for i in range(len(DOWNLOADS)):
+    #     sys.stdout.write('\x1b[1A')  # Move cursor up one line
+    #     sys.stdout.write('\x1b[2K')
+    #     sys.stdout.flush()
+    # os.system('cls')
+    print()
+    print("Program terminated.")
+    sys.exit(0)
 
 def setup_signal_handler():
     signal.signal(signal.SIGINT, disconnect)
@@ -112,8 +106,9 @@ def respond_to_server(conn):
                     if tag == "OK":
                         file_name, file_size = message.split()[2:]
                         file_size = int(file_size)
-
-                        progress_bar = tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024, desc=file_name, colour='green')
+                        # + ' ' * 3
+                        bar_format = '{l_bar}{bar}{r_bar}' 
+                        progress_bar = tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024, desc= ' ' * 2 + file_name, colour='green', bar_format=bar_format)
                         completed = False
                         for file in DOWNLOADS:
                             if file_name == file[0]:
@@ -146,7 +141,14 @@ def respond_to_server(conn):
 
                     file_name = message.split()[1]
                     
-                    with open(f"receive_{file_name}", 'ab') as file:
+                    output_folder = "output"
+
+                    if not os.path.exists(output_folder):
+                        os.makedirs(output_folder)
+
+                    file_path = os.path.join(output_folder, f"receive_{file_name}")
+
+                    with open(file_path, 'ab') as file:
                         file.write(data)
 
                     for file in DOWNLOADS:
@@ -158,7 +160,18 @@ def respond_to_server(conn):
                 elif method == "ERR":
                     message = message.decode(FORMAT)
                     file_name = message.split()[1]
-                    print(f"Error: File '{file_name}' does not exist on the server.")
+
+                    flag = False
+
+                    for file in DOWNLOADS:
+                        if file[2] == True:
+                            flag = True
+                            break
+                            
+                    if flag:
+                        tqdm.write(f"\n  [ERROR] <{file_name}> does not exist on the server.\n")
+                    else: 
+                        tqdm.write(f"  [ERROR] <{file_name}> does not exist on the server.\n")
                     
     except Exception as e:
         if not shutdown_event.is_set():
@@ -167,6 +180,7 @@ def respond_to_server(conn):
 
 def process_input_file(conn):
     global PROCESSED_TRACKER
+    global FILE_LIST
 
     with open(INPUT_FILE, 'r') as file:
         contents = [line.rstrip() for line in file]
@@ -176,12 +190,37 @@ def process_input_file(conn):
         for line in contents[start_downloading_from:]:
             if shutdown_event.is_set():
                 break
+
             file_name, priority = line.split(DELIMITER, 1)
-            if file_name not in [item[0] for item in DOWNLOADS]:
+            
+            if file_name in FILE_LIST and file_name not in [item[0] for item in DOWNLOADS]: 
                 request_file(conn, file_name, priority)
                 DOWNLOADS.append([file_name, None, None])
+            elif file_name not in FILE_LIST:
+                flag = False
+
+                for file in DOWNLOADS:
+                    if file[2] == True:
+                        flag = True
+                        break
+                        
+                if flag:
+                    tqdm.write(f"\n  [ERROR] <{file_name}> does not exist on the server.\n")
+                else: 
+                    tqdm.write(f"  [ERROR] <{file_name}> does not exist on the server.\n")
             else:
-                print("\nFile already requested!")
+                flag = False
+
+                for file in DOWNLOADS:
+                    if file[2] == True:
+                        flag = True
+                        break
+                        
+                if flag:
+                    tqdm.write(f"\n  [WARNING] <{file_name}> has already been requested!\n")
+                else: 
+                    tqdm.write(f"  [WARNING] <{file_name}> has already been requested!\n")
+                
             PROCESSED_TRACKER += 1
 
 def update_input_file(conn):
@@ -194,21 +233,29 @@ def update_input_file(conn):
 
 def initiate_connection():
     global PROCESSED_TRACKER
+    global FILE_LIST
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.connect((HOST, PORT))
+        print(f"Connected to server at {(HOST, PORT)}\n")
+
         setup_signal_handler()
-        # setup_signal_handler(client)
 
-        FILE_LIST = get_file_list(client)
+        file_list = get_file_list(client)
 
-        if FILE_LIST:
-            print("Available files on server:")
-            print(FILE_LIST + '\n')
+        if file_list:
+            print("Available files on server:\n")
+            for line in file_list.splitlines():
+                print('  - ' + line)
+
+            print()
+            
+            FILE_LIST = [file.split()[0] for file in file_list.splitlines()]
         else:
             print("Failed to retrieve file list.")
             return
 
+        print("Home:\n")
         server_handler = threading.Thread(target=respond_to_server, args=[client])
         server_handler.start()
 
@@ -219,15 +266,8 @@ def initiate_connection():
             while not shutdown_event.is_set():
                 time.sleep(0.1)  # Sleep to reduce CPU usage
         except (KeyboardInterrupt, SystemExit):
-            for file in DOWNLOADS:
-                file[1].close()
-                sys.stdout.write('\x1b[1A')  # Move cursor up one line
-                sys.stdout.write('\x1b[2K')
-                sys.stdout.flush()
-            print("Exiting...")
-            sys.exit(0)
+            shutdown_event.set()
         
-        shutdown_event.set()
         input_file_handler.join()
         server_handler.join()
 
